09daa96ff4cd966759f2d30b66bebe16afaf00d6
Alessandro <alessandro.giangreco@gmail.com>
2019-03-08 15:32:11 +0100

    rest completate

--- ImportSportelliCap/src/main/java/com/poste/si/importsportellicap/controller/ImportSportelliCapController.java
+++ ImportSportelliCap/src/main/java/com/poste/si/importsportellicap/controller/ImportSportelliCapController.java
@@ -7,7 +7,6 @@ package com.poste.si.importsportellicap.controller;
 
 import com.poste.si.importsportellicap.model.SportelliCap;
 import com.poste.si.importsportellicap.service.SportelliCapService;
-import java.util.HashMap;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
