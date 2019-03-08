615719344090dd7f6705d0a7b5a54fef8f9d8156
Alessandro <alessandro.giangreco@gmail.com>
2019-03-08 14:02:33 +0100

    add some log

--- ImportSportelliCap/src/main/java/com/poste/si/importsportellicap/scheduledimport/ImportSportelliCapTask.java
+++ ImportSportelliCap/src/main/java/com/poste/si/importsportellicap/scheduledimport/ImportSportelliCapTask.java
@@ -5,6 +5,8 @@
  */
 package com.poste.si.importsportellicap.scheduledimport;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -18,11 +20,12 @@ import org.springframework.stereotype.Component;
 @Component
 public class ImportSportelliCapTask {
 
-    final static Logger logger = LoggerFactory.getLogger(ImportSportelliCapTask.class);
+    private final static Logger logger = LoggerFactory.getLogger(ImportSportelliCapTask.class);
+    private final static SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
     
     @Scheduled(fixedRate = 5000)
     public void doImportSportelliCapTask() {
-        logger.info("scheduled ... ");
+        logger.info("The time is now {}", dateFormat.format(new Date()));
     }
      
 }
