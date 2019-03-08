0508293c957df05427d4875881f2f05b3e817ee1
Alessandro <alessandro.giangreco@gmail.com>
2019-03-08 12:31:28 +0100

    enable swagger

---
+++ ImportSportelliCap/src/main/java/com/poste/si/importsportellicap/controller/ImportSportelliCapController.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.poste.si.importsportellicap.controller;
+
+import java.util.HashMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Alex
+ */
+@RestController
+public class ImportSportelliCapController {
+
+    @RequestMapping(value = "/status", produces = "application/json")
+    @ResponseBody
+    public HashMap<Integer, String> getStatus() {
+        HashMap<Integer, String> status = new HashMap<>();
+        status.put(0, "Success");
+        return status;
+    }
+
+}
