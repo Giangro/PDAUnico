bfe06b3ba9aa0e903736a957377e590ceb9c9933
Alessandro <alessandro.giangreco@gmail.com>
2019-03-08 21:54:51 +0100

    Anagrafica Sportelli rest completate

---
+++ ImportAnagSportelli/src/main/java/com/poste/si/importanagsportelli/service/AnagSportelliService.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.poste.si.importanagsportelli.service;
+
+import com.poste.si.importanagsportelli.dao.AnagSportelliDao;
+import com.poste.si.importanagsportelli.model.AnagSportelli;
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author Alex
+ */
+@Service
+public class AnagSportelliService {
+
+    final static Logger logger = LoggerFactory.getLogger(AnagSportelliService.class);
+
+    @Autowired
+    AnagSportelliDao anagSportelliDao;
+
+    public List<AnagSportelli> findAllAnagSportelliService() {
+        return anagSportelliDao.findAll();
+    }
+
+    public List<AnagSportelli> findInfoByCapService(String cap) {
+        return anagSportelliDao.findInfoByCap(cap);
+    }
+
+}
